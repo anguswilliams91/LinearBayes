8cdac217ee632800086d0537c63602e66e6ec2e9
anguswilliams91 <anguswilliams91@gmail.com>
2016-06-10 11:07:38 +0100

    moved stuff around to make into package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='linear_bayes',
+      version='0.1',
+      description='Bayesian linear regression',
+      url='https://github.com/anguswilliams91/LinearBayes',
+      author='Angus Williams',
+      author_email='anguswilliams91@gmail.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
